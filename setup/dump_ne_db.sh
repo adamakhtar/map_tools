a9de4894e0f8cc3199802503cd19f0c699ea8884
Adam Akhtar <adamsubscribe@googlemail.com>
2022-04-02 21:30:49 +0900

    change: change pgdump to use compression level 1 instead of 6 - benefits of 6 do not outweigh time costs

--- setup/dump_ne_db.sh
+++ setup/dump_ne_db.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-sudo pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -F c -b -v -f $BASE_NE_DUMP_PATH $NE_DB_NAME
+sudo pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -F c -Z1 -b -v -f $BASE_NE_DUMP_PATH $NE_DB_NAME
--- setup/dump_osm_db.sh
+++ setup/dump_osm_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -F c -b -v -f $BASE_OSM_DUMP_PATH $OSM_DB_NAME
+sudo pg_dump -h $DB_HOST -p $DB_PORT -U $DB_USER -F c -Z1 -b -v -f $BASE_OSM_DUMP_PATH $OSM_DB_NAME
